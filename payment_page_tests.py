f66f9092f9729dbc756b95bb4f2768284d31da82
armoz14 <38024713+armorasha@users.noreply.github.com>
2019-07-16 21:55:19 +0930

    2nd day Commit 2

--- payment_page_tests.py
+++ payment_page_tests.py
@@ -1,5 +1,8 @@
 import unittest
 import time
+
+import mysql.connector
+
 from selenium.webdriver.common.keys import Keys  # for sending special keys like arrow down
 
 from selenium.common.exceptions import NoSuchElementException
@@ -31,28 +34,20 @@ class PaymentTestCase(unittest.TestCase):
         self.assertNotIn("payment.php", driv1.current_url)
         # No direct access to payment_page? by checking the url has no 'payment.php'
 
-    def test_ifPlaceOrderBtnWorks_guest(self): # condition: if a member logged in, this fails
+    def test_ifPlaceOrderBtnWorks_guest(self): # condition: if a member already logged in, this test fails
         driv = self.driver
-
-        time.sleep(2)
 
         select = driv.find_element_by_xpath('//*[@id="collapse-1"]/div/div/div[3]/div/form/button')
         select.click()
         # Selects Kotto Pollo from order page and goes to select page
 
-        time.sleep(2)
-
         add_qty = driv.find_element_by_xpath('/html/body/form/div/div/button[2]')
         add_qty.click()
         # adds one to qty
 
-        time.sleep(2)
-
         add_to_cart = driv.find_element_by_xpath('/html/body/form/div/div/button[3]')
         add_to_cart.click()
         # adds Kotto Pollo to cart
-
-        time.sleep(2)
 
         proceed_to_checkout = driv.find_element_by_xpath('/html/body/div/div/form/button')
         proceed_to_checkout.click()
@@ -61,8 +56,6 @@ class PaymentTestCase(unittest.TestCase):
         # self.assertIn("payment.php", driv.current_url)
         # Arrived in payment_page? by checking the url has 'payment.php'
 
-        time.sleep(3)
-
         # Arrived in payment_page
         guest1 = {"guestEmail": "armi@yahee.ccc", "cust_name": "Raji Pali", "street": "100 Sth rd", "suburb": "Roarx",
                   "postcode": "5559", "phone": "0444111222"}
@@ -70,12 +63,56 @@ class PaymentTestCase(unittest.TestCase):
         for key, value in guest1.items():
             driv.find_element_by_id(key).send_keys(value)
 
-        self.assertIn("$13.90", driv.page_source)
+        self.assertIn("Total Amount $13.90", driv.page_source)
+        # checking to see if total amount received
 
+        cash_radio_button = driv.find_element_by_xpath('/html/body/div[1]/form/div[3]/div[1]/div[4]/label/input')
+        cash_radio_button.click()
 
-        self.assertIn("payment.php", driv.current_url)
+        time.sleep(2) # AJAX wait time or something. Leave this.
+
+        place_the_order_button = driv.find_element_by_xpath('//*[@id="captchaBtn2"]')
+        # place_the_order_button.click() # places the order.
+
+        self.assertIn("Order placed. Your order number is", driv.page_source)
+        # checking to see if order is placed
+
+    def test_ifPaymentLoginWorks_guest(self): # condition: if a member already logged in, this test fails
+        driv = self.driver
+
+        select = driv.find_element_by_xpath('//*[@id="collapse-1"]/div/div/div[3]/div/form/button')
+        select.click()
+        # Selects Kotto Pollo from order page and goes to select page
+
+        add_qty = driv.find_element_by_xpath('/html/body/form/div/div/button[2]')
+        add_qty.click()
+        # adds one to qty
+
+        add_to_cart = driv.find_element_by_xpath('/html/body/form/div/div/button[3]')
+        add_to_cart.click()
+        # adds Kotto Pollo to cart
+
+        proceed_to_checkout = driv.find_element_by_xpath('/html/body/div/div/form/button')
+        proceed_to_checkout.click()
+        # clicks proceed to checkout button
+
+        # self.assertIn("payment.php", driv.current_url)
+        # Arrived in payment_page? by checking the url has 'payment.php'
+
+        # Arrived in payment_page
+        member1 = {"email": "cust14@hmail.com", "psw": "14ssssss"}
+
+        for key, value in member1.items():
+            driv.find_element_by_id(key).send_keys(value)
+
+        member_login_btn = driv.find_element_by_xpath('//*[@id="captchaBtn1"]')
+        member_login_btn.click()
+        # clicks member login button
+
+        self.assertIn("Vinod Kumar", driv.page_source)
+        # checking to see if total amount received
 
     def tearDown(self):
-        time.sleep(10)
+        time.sleep(3)
         self.driver.close()
 
